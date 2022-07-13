4fb686acc3ddd6e8f8bf8ec848511bdaac4f6582
duchuynh1201 <102602729+duchuynh1201@users.noreply.github.com>
2022-07-13 20:51:42 +0700

    New

--- Test in home/Source.cpp
+++ Test in home/Source.cpp
@@ -2,27 +2,37 @@
 
 using namespace std;
 
+struct Node {
+	int data;
+	Node* next;
+};
+
+Node* createListNode(int* arr, int n) {
+	Node* list = nullptr, * cur = list;
+	for (int i = 0; i < n; i++) {
+		if (cur == nullptr)
+			cur = new Node;
+		else {
+			cur->next = new Node;
+			cur = cur->next;
+		}
+		cur->data = arr[i];
+	}
+}
+
+void displayListNode(Node* list) {
+	Node* cur = list;
+	while (cur != nullptr) {
+		cout << cur->data << " ";
+		cur = cur->next;
+	}
+}
+
 int main()
 {
-	int** a;
-	a = new int*[5];
-	for (int i = 0; i < 5; i++)
-	{
-		a[i] = new int[7];
-	}
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 7; j++)
-		{
-			a[i][j] = 1802;
-		}
-	}
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 7; j++)
-		{
-			cout << " " << a[i][j];
-		}
-		cout << endl;
-	}
+	int arr[] = { 1,2,3,4,5,6,7 };
+	Node* list = createListNode(arr, 7);
+	displayListNode(list);
+
+	return 0;
 }
